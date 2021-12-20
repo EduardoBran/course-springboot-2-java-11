5db78208d61a79a50b792caf2e894a873a06172e
Eduardo Brandao <eduardo.ads1814@gmail.com>
2021-12-20 12:28:28 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,21 +1,37 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController //Para falar que está classe é um recurso web que é implementado por um controlador Rest
 @RequestMapping(value = "/users") //nome para o recurso
 public class UserResource {
 	
+	@Autowired //dependência para o service
+	private UserService service;
+	
 	//método para testar se está funcionando o recurso users
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
 		
-		User u = new User(1L, "maria", "maria@gmail.com", "99999999", "123456789");
-		return ResponseEntity.ok().body(u);
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}") //indica que vai aceitar um ID dentro da url
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}	
 }
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,32 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+//@Component //regitra a classe como um componente do Spring
+@Service //para ficar mais semanticamente especifico
+public class UserService {
+	
+	@Autowired //faz com o spring faça a dependencia de forma transparente
+	private UserRepository repository;
+	
+	//método para retornar todos os usuários do banco de dados
+	public List<User> findAll(){
+		
+		return repository.findAll(); //aqui o repository ja oferece varias operações
+	}	
+	
+	//método para retorna por ID
+	public User findById(Long id) {
+		
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
